c6ca34b505700de84d73e0c0a2c8b6523b2ae3d8
Thirki <cicero.santos62@etec.sp.gov.br>
2021-12-17 17:21:16 -0300

    Fix: responsividade de textos

--- src/components/About.js
+++ src/components/About.js
@@ -3,7 +3,7 @@ import './About.css'
 
 export default function About() {
   return (
-    <main id="About">
+    <main id="About"> 
       <h1>Sobre mim</h1>
       <p>
       Sou novo no mercado de tecnologia e estudo desenvolvimento full-stack na escola Trybe de programação, por ser uma pessoa autodidata eu já tenho uma bagagem grande de desenvolvimento front-end, realizei alguns bootcamps, cursos e  projetos sozinho nessa área, e acredito estar mais do que preparado para embarcar no mercado de trabalho.
