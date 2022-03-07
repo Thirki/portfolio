9e7006388cd0026c2406dd187d92c77b85e262ca
Thirki <cicero.santos62@etec.sp.gov.br>
2022-03-07 13:23:38 -0300

    Feat: atualizacao da descricao

--- src/components/About.js
+++ src/components/About.js
@@ -6,7 +6,11 @@ export default function About() {
     <main id="About"> 
       <h1>Sobre mim</h1>
       <p>
-      Sou novo no mercado de tecnologia e estudo desenvolvimento full-stack na escola Trybe de programação, por ser uma pessoa autodidata eu já tenho uma bagagem grande de desenvolvimento front-end, realizei alguns bootcamps, cursos e  projetos sozinho nessa área, e acredito estar mais do que preparado para embarcar no mercado de trabalho.
+        Atualmente atuo como desenvolvedor front-end junior na Dopster, e estudo
+        desenvolvimento fullstack na escola Trybe de programação, procuro cada vez mais 
+        me desenvolver na área da programação e alem dos conhecimentos técnicos em programação,
+        eu gosto muito de estudar sobre gerenciamento e liderança e busco sempre desenvolver minhas
+        soft-skills.
       </p>
     </main>
   )
